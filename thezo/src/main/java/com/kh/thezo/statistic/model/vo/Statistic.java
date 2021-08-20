dda9e4d436c273ad6708fae4e648ba9daa24acd1
Jaewon-S <ypd00442@gmail.com>
2021-08-20 14:37:39 +0900

    fix: adminMain
    fix : delete StatMonths VO
          then using existed Statistic VO Class

--- thezo/src/main/java/com/kh/thezo/statistic/model/vo/Statistic.java
+++ thezo/src/main/java/com/kh/thezo/statistic/model/vo/Statistic.java
@@ -1,5 +1,23 @@
 package com.kh.thezo.statistic.model.vo;
+
+import java.util.ArrayList;
+
+import com.kh.thezo.attendance.model.vo.Attendance;
+import com.kh.thezo.member.model.vo.Member;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
 //@author Jaewon.s
+@NoArgsConstructor
+@Getter	@Setter
+@ToString
 public class Statistic {
+	private int months;
+	
+	ArrayList<Member> memStat = new ArrayList<>();
+	ArrayList<Attendance> attendanceList = new ArrayList<>();
 
 }
